501d773a02a73a005a86c96ffc009dc3586b192f
CarlosAcharly <charlyc1502@gmail.com>
2026-01-15 02:27:41 -0600

    fix de editar dieta

--- Almacen_FH/dietas/templatetags/dietas_extras.py
+++ Almacen_FH/dietas/templatetags/dietas_extras.py
@@ -4,8 +4,10 @@ register = template.Library()
 
 @register.filter
 def get_item(dic, key):
-    return dic.get(key)
+    """Obtiene un valor de un diccionario por clave, o 0 si no existe"""
+    return dic.get(key, 0)  # Devuelve 0 si la clave no existe
 
 @register.filter(name='add_class')
 def add_class(field, css):
+    """Añade una clase CSS a un campo de formulario"""
     return field.as_widget(attrs={'class': css})
